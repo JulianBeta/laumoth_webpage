97b54d79471141ed53ba080fd2b145f52aa07b47
JulianBeta <carramplancha@gmail.com>
2024-08-26 07:54:41 -0500

    Page Header with styles

--- src/general_components/Header.js
+++ src/general_components/Header.js
@@ -1,7 +1,7 @@
 // src/Header.js
 import React, { useState } from 'react';
 import './../styles/Header.css'
-import logo from './../../src/logo.svg'; // Ensure you have a logo image in your src directory
+import logo from './../Grupo 1.png' // Ensure you have a logo image in your src directory
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -20,11 +20,11 @@ const Header = () => {
         <div className="header-desktop">
           <nav>
             <ul>
-              <li><a href="#link1">Link 1</a></li>
-              <li><a href="#link2">Link 2</a></li>
+              <li><a href="#link1">Inicio</a></li>
+              <li><a href="#link2">Galeria</a></li>
               <li className="logo"><a href="#home"><img src={logo} alt="Logo" /></a></li>
-              <li><a href="#link3">Link 3</a></li>
-              <li><a href="#link4">Link 4</a></li>
+              <li><a href="#link3">Tienda</a></li>
+              <li><a href="#link4">Contacto</a></li>
             </ul>
           </nav>
         </div>
@@ -42,10 +42,10 @@ const Header = () => {
             </div>
             <nav>
               <ul>
-                <li><a href="#link1" onClick={closeMenu}>Link 1</a></li>
-                <li><a href="#link2" onClick={closeMenu}>Link 2</a></li>
-                <li><a href="#link3" onClick={closeMenu}>Link 3</a></li>
-                <li><a href="#link4" onClick={closeMenu}>Link 4</a></li>
+                <li><a href="#link1" onClick={closeMenu}>Inicio</a></li>
+                <li><a href="#link2" onClick={closeMenu}>Galeria</a></li>
+                <li><a href="#link3" onClick={closeMenu}>Tienda</a></li>
+                <li><a href="#link4" onClick={closeMenu}>Contacto</a></li>
               </ul>
             </nav>
           </div>
