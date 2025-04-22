9b66bf3ff514eafcdba9f09339395c8b38cd8c16
JulianBeta <carramplancha@gmail.com>
2025-04-22 09:52:21 -0500

    Gallery, footer and general title section styles

--- src/general_components/Footer.jsx
+++ src/general_components/Footer.jsx
@@ -3,17 +3,13 @@ import '../styles/Footer.css'
  
 const Footer = () => {
     return (
-        <div className="footercontainer">
-                     
-                     <div className="primerrenglon">
-<div className="seccionprimerrenglon">LAURA MOTH.</div  > <div className="seccionprimerrenglon">ALL RIGHTS RESERVED. </div> <div className="seccionprimerrenglon">© </div><div className="seccionprimerrenglon"> 2023 </div>
-
-                     </div>
-                    
-                     <div className="segundorenglon">
-made by Betico
-
-                     </div>
+        <div className="footercontainer">        
+            <div className="primerrenglon">
+                <div className="seccionprimerrenglon">LAURA MOTH.</div> 
+                <div className="seccionprimerrenglon">ALL RIGHTS RESERVED.</div> 
+                <div className="seccionprimerrenglon">© 2023</div>
+            </div>          
+        <div className="segundorenglon">made by CODEPEC SAS</div>
         </div>
     );
 };
